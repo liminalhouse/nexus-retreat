5b4dc337a6faf67d4ef6962b84755d7d19ee7054
engineear1 <engineear@proton.me>
2025-08-28 18:32:07 -0500

    minor update

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ import withPWA from 'next-pwa'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true, // Enable React strict mode for improved error handling
-    swcMinify: true, // Enable SWC minification for improved performance
     compiler: {
         removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
     },
